22416ace8d5f97d5440924621dd994b3b262cfc7
Pantelis Antoniou <pantelis.antoniou@konsulko.com>
2015-03-17 15:25:46 +0200

    of: overlay: Global sysfs enable attribute
    A throw once master enable switch to protect against any further overlay
    changes if the administrator desires so.
    
    A kernel command line option is provided as well.
    
    Signed-off-by: Pantelis Antoniou <pantelis.antoniou@konsulko.com>
    [geert: Rebase on top of commit 067c098766c6af66 ("of: overlay: rework overlay apply and remove kfree()s") in v5.19-rc1]
    [geert: Extend to overlay removals]
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

--- drivers/of/overlay.c
+++ drivers/of/overlay.c
@@ -21,6 +21,7 @@
 #include <linux/err.h>
 #include <linux/idr.h>
 #include <linux/sysfs.h>
+#include <linux/atomic.h>
 
 #include "of_private.h"
 
@@ -80,6 +81,16 @@ struct overlay_changeset {
 	struct of_changeset cset;
 	struct kobject kobj;
 };
+
+/* master enable switch; once set to 0 can't be re-enabled */
+static atomic_t ov_enable = ATOMIC_INIT(1);
+
+static int __init of_overlay_disable_setup(char *str __always_unused)
+{
+	atomic_set(&ov_enable, 0);
+	return 1;
+}
+__setup("of_overlay_disable", of_overlay_disable_setup);
 
 /* flags are sticky - once set, do not reset */
 static int devicetree_state_flags;
@@ -884,6 +895,35 @@ static void overlay_changeset_release(struct kobject *kobj)
 	kfree(ovcs);
 }
 
+static ssize_t enable_show(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%d\n", atomic_read(&ov_enable));
+}
+
+static ssize_t enable_store(struct kobject *kobj,
+		struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	bool new_enable;
+
+	ret = strtobool(buf, &new_enable);
+	if (ret != 0)
+		return ret;
+	/* if we've disabled it, no going back */
+	if (atomic_read(&ov_enable) == 0)
+		return -EPERM;
+	atomic_set(&ov_enable, (int)new_enable);
+	return count;
+}
+
+static struct kobj_attribute enable_attr = __ATTR_RW(enable);
+
+static const struct attribute *overlay_global_attrs[] = {
+	&enable_attr.attr,
+	NULL
+};
+
 static struct kobj_type overlay_changeset_ktype = {
 	.release = overlay_changeset_release,
 };
@@ -995,6 +1035,10 @@ int of_overlay_fdt_apply(const void *overlay_fdt, u32 overlay_fdt_size,
 	struct overlay_changeset *ovcs;
 
 	*ret_ovcs_id = 0;
+
+	/* administratively disabled */
+	if (!atomic_read(&ov_enable))
+		return -EPERM;
 
 	if (devicetree_corrupt()) {
 		pr_err("devicetree state suspect, refuse to apply overlay\n");
@@ -1207,6 +1251,10 @@ int of_overlay_remove(int *ovcs_id)
 	struct overlay_changeset *ovcs;
 	int ret, ret_apply, ret_tmp;
 
+	/* administratively disabled */
+	if (!atomic_read(&ov_enable))
+		return -EPERM;
+
 	if (devicetree_corrupt()) {
 		pr_err("suspect devicetree state, refuse to remove overlay\n");
 		ret = -EBUSY;
@@ -1299,9 +1347,14 @@ EXPORT_SYMBOL_GPL(of_overlay_remove_all);
 /* called from of_init() */
 int of_overlay_init(void)
 {
+	int rc;
+
 	ov_kset = kset_create_and_add("overlays", NULL, &of_kset->kobj);
 	if (!ov_kset)
 		return -ENOMEM;
 
-	return 0;
+	rc = sysfs_create_files(&ov_kset->kobj, overlay_global_attrs);
+	WARN(rc, "%s: error adding global attributes\n", __func__);
+
+	return rc;
 }
