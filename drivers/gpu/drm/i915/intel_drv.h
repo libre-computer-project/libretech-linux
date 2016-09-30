5bc0fc9a545641b19bdfa32911fd755b41b428ab
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2016-09-30 11:58:42 +0200

    drm/i915: Fix build when !CONFIG_DEBUG_FS
    Add stub for intel_crtc_set_crc_source() and fix arguments of stub for
    intel_display_crc_init().
    
    Signed-off-by: Tomeu Vizoso <tomeu.vizoso@collabora.com>
    Fixes: 21165bd933ac ("drm/i915/debugfs: Move out pipe CRC code")
    Fixes: 13fa0253d97a ("drm/i915: Use new CRC debugfs API")
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Link: http://patchwork.freedesktop.org/patch/msgid/1475229522-24113-1-git-send-email-tomeu.vizoso@collabora.com

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1837,8 +1837,14 @@ void intel_color_load_luts(struct drm_crtc_state *crtc_state);
 /* intel_pipe_crc.c */
 int intel_pipe_crc_create(struct drm_minor *minor);
 void intel_pipe_crc_cleanup(struct drm_minor *minor);
+#ifdef CONFIG_DEBUG_FS
 int intel_crtc_set_crc_source(struct drm_crtc *crtc, const char *source_name,
 			      size_t *values_cnt);
+#else
+static inline int intel_crtc_set_crc_source(struct drm_crtc *crtc,
+					    const char *source_name,
+					    size_t *values_cnt) { return 0; }
+#endif
 extern const struct file_operations i915_display_crc_ctl_fops;
 
 #endif /* __INTEL_DRV_H__ */
