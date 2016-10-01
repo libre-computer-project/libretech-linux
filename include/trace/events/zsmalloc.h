57035a9ff5c2d9439551dca4a065a38826a2ee89
Ganesh Mahendran <opensource.ganesh@gmail.com>
2016-10-01 11:32:11 +1000

    mm/zsmalloc: add per-class compact trace event
    add per-class compact trace event to get number of migrated objects
    and number of freed pages.
    
    trace log is like below:
    
                bash-3863  [001] ....   141.791366: zs_compact_start: pool zram0
                bash-3863  [001] ....   141.791372: zs_compact: class 254: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791375: zs_compact: class 202: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791385: zs_compact: class 190: 1 objects migrated, 3 pages freed
                bash-3863  [001] ....   141.791393: zs_compact: class 168: 2 objects migrated, 2 pages freed
                bash-3863  [001] ....   141.791396: zs_compact: class 151: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791407: zs_compact: class 144: 5 objects migrated, 4 pages freed
                bash-3863  [001] ....   141.791427: zs_compact: class 126: 8 objects migrated, 8 pages freed
                bash-3863  [001] ....   141.791433: zs_compact: class 111: 1 objects migrated, 4 pages freed
                bash-3863  [001] ....   141.791459: zs_compact: class 107: 18 objects migrated, 12 pages freed
                bash-3863  [001] ....   141.791487: zs_compact: class 100: 18 objects migrated, 16 pages freed
                bash-3863  [001] ....   141.791509: zs_compact: class  94: 18 objects migrated, 9 pages freed
                bash-3863  [001] ....   141.791560: zs_compact: class  91: 44 objects migrated, 24 pages freed
                bash-3863  [001] ....   141.791605: zs_compact: class  83: 35 objects migrated, 20 pages freed
                bash-3863  [001] ....   141.791616: zs_compact: class  76: 8 objects migrated, 4 pages freed
                bash-3863  [001] ....   141.791644: zs_compact: class  74: 21 objects migrated, 9 pages freed
                bash-3863  [001] ....   141.791665: zs_compact: class  71: 18 objects migrated, 10 pages freed
                bash-3863  [001] ....   141.791736: zs_compact: class  67: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791763: zs_compact: class  66: 22 objects migrated, 8 pages freed
                bash-3863  [001] ....   141.791820: zs_compact: class  62: 18 objects migrated, 6 pages freed
                bash-3863  [001] ....   141.791826: zs_compact: class  58: 1 objects migrated, 4 pages freed
                bash-3863  [001] ....   141.791829: zs_compact: class  57: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791834: zs_compact: class  54: 2 objects migrated, 2 pages freed
    ...
                bash-3863  [001] ....   141.791952: zs_compact: class   4: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791964: zs_compact: class   3: 14 objects migrated, 1 pages freed
                bash-3863  [001] ....   141.791966: zs_compact: class   2: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791968: zs_compact: class   1: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791971: zs_compact: class   0: 0 objects migrated, 0 pages freed
                bash-3863  [001] ....   141.791973: zs_compact_end: pool zram0: 155 pages compacted
    
    Also this patch changes trace_zsmalloc_compact_start[end] to
    trace_zs_compact_start[end] to keep function naming consistent with others
    in zsmalloc.
    
    Link: http://lkml.kernel.org/r/1467882338-4300-8-git-send-email-opensource.ganesh@gmail.com
    Signed-off-by: Ganesh Mahendran <opensource.ganesh@gmail.com>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/trace/events/zsmalloc.h
+++ include/trace/events/zsmalloc.h
@@ -7,7 +7,7 @@
 #include <linux/types.h>
 #include <linux/tracepoint.h>
 
-TRACE_EVENT(zsmalloc_compact_start,
+TRACE_EVENT(zs_compact_start,
 
 	TP_PROTO(const char *pool_name),
 
@@ -25,29 +25,49 @@ TRACE_EVENT(zsmalloc_compact_start,
 		  __entry->pool_name)
 );
 
-TRACE_EVENT(zsmalloc_compact_end,
+TRACE_EVENT(zs_compact_end,
 
-	TP_PROTO(const char *pool_name, unsigned long pages_compacted,
-			unsigned long pages_total_compacted),
+	TP_PROTO(const char *pool_name, unsigned long pages_compacted),
 
-	TP_ARGS(pool_name, pages_compacted, pages_total_compacted),
+	TP_ARGS(pool_name, pages_compacted),
 
 	TP_STRUCT__entry(
 		__field(const char *, pool_name)
 		__field(unsigned long, pages_compacted)
-		__field(unsigned long, pages_total_compacted)
 	),
 
 	TP_fast_assign(
 		__entry->pool_name = pool_name;
 		__entry->pages_compacted = pages_compacted;
-		__entry->pages_total_compacted = pages_total_compacted;
 	),
 
-	TP_printk("pool %s: %ld pages compacted(total %ld)",
+	TP_printk("pool %s: %ld pages compacted",
 		  __entry->pool_name,
-		  __entry->pages_compacted,
-		  __entry->pages_total_compacted)
+		  __entry->pages_compacted)
+);
+
+TRACE_EVENT(zs_compact,
+
+	TP_PROTO(int class, unsigned long nr_migrated_obj, unsigned long nr_freed_pages),
+
+	TP_ARGS(class, nr_migrated_obj, nr_freed_pages),
+
+	TP_STRUCT__entry(
+		__field(int, class)
+		__field(unsigned long, nr_migrated_obj)
+		__field(unsigned long, nr_freed_pages)
+	),
+
+	TP_fast_assign(
+		__entry->class = class;
+		__entry->nr_migrated_obj = nr_migrated_obj;
+		__entry->nr_freed_pages = nr_freed_pages;
+	),
+
+	TP_printk("class %3d: %ld objects migrated, %ld pages freed",
+		  __entry->class,
+		  __entry->nr_migrated_obj,
+		  __entry->nr_freed_pages)
 );
 
 #endif /* _TRACE_ZSMALLOC_H */
