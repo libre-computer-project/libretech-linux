c06599d4b855a72202c0fad26110eefe085ca690
Michael S. Tsirkin <mst@redhat.com>
2017-01-12 23:36:32 +0200

    virtio_mmio: expose header to userspace
    It's handy for userspace emulators like QEMU.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- drivers/virtio/virtio_mmio.c
+++ drivers/virtio/virtio_mmio.c
@@ -69,7 +69,7 @@
 #include <linux/spinlock.h>
 #include <linux/virtio.h>
 #include <linux/virtio_config.h>
-#include <linux/virtio_mmio.h>
+#include <uapi/linux/virtio_mmio.h>
 #include <linux/virtio_ring.h>
 
 
