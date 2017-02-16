fb5d7dc70ee9160ed170caa1cb0e9948654edcca
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2017-02-16 13:27:14 +0000

    pinctrl: samsung: Register pinctrl before GPIO
    If we request a GPIO hog, then gpiochip_add_data will attempt to request
    some of its own GPIOs. The driver also uses gpiochip_generic_request
    which means that for any GPIO request to succeed the pinctrl needs to be
    registered. Currently however the driver registers the GPIO and then the
    pinctrl meaning all GPIO hog requests will fail, which then in turn causes
    the whole driver to fail probe. Fix this up by ensuring we register the
    pinctrl first. Although currently there are no users of GPIO hogs in
    mainline there are plenty of Samsung based boards that are widely used
    for development purposes of other hardware. Indeed we hit this issue
    whilst attaching some additional hardware to an Arndale system.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Reviewed-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/pinctrl/samsung/pinctrl-samsung.c
+++ drivers/pinctrl/samsung/pinctrl-samsung.c
@@ -893,6 +893,19 @@ static int samsung_pinctrl_register(struct platform_device *pdev,
 	return 0;
 }
 
+/* unregister the pinctrl interface with the pinctrl subsystem */
+static int samsung_pinctrl_unregister(struct platform_device *pdev,
+				      struct samsung_pinctrl_drv_data *drvdata)
+{
+	struct samsung_pin_bank *bank = drvdata->pin_banks;
+	int i;
+
+	for (i = 0; i < drvdata->nr_banks; ++i, ++bank)
+		pinctrl_remove_gpio_range(drvdata->pctl_dev, &bank->grange);
+
+	return 0;
+}
+
 static const struct gpio_chip samsung_gpiolib_chip = {
 	.request = gpiochip_generic_request,
 	.free = gpiochip_generic_free,
@@ -937,19 +950,6 @@ fail:
 	for (--i, --bank; i >= 0; --i, --bank)
 		gpiochip_remove(&bank->gpio_chip);
 	return ret;
-}
-
-/* unregister the gpiolib interface with the gpiolib subsystem */
-static int samsung_gpiolib_unregister(struct platform_device *pdev,
-				      struct samsung_pinctrl_drv_data *drvdata)
-{
-	struct samsung_pin_bank *bank = drvdata->pin_banks;
-	int i;
-
-	for (i = 0; i < drvdata->nr_banks; ++i, ++bank)
-		gpiochip_remove(&bank->gpio_chip);
-
-	return 0;
 }
 
 /* retrieve the soc specific data */
@@ -1062,13 +1062,13 @@ static int samsung_pinctrl_probe(struct platform_device *pdev)
 			return PTR_ERR(drvdata->retention_ctrl);
 	}
 
-	ret = samsung_gpiolib_register(pdev, drvdata);
+	ret = samsung_pinctrl_register(pdev, drvdata);
 	if (ret)
 		return ret;
 
-	ret = samsung_pinctrl_register(pdev, drvdata);
+	ret = samsung_gpiolib_register(pdev, drvdata);
 	if (ret) {
-		samsung_gpiolib_unregister(pdev, drvdata);
+		samsung_pinctrl_unregister(pdev, drvdata);
 		return ret;
 	}
 
