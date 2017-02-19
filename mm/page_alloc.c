b8b39c9b552e252361a955721c76b9cd288fd87e
Mel Gorman <mgorman@techsingularity.net>
2017-02-19 20:00:19 +1100

    mm, page_alloc: only use per-cpu allocator for irq-safe requests -fix
    preempt_enable_no_resched() was used based on review feedback that had
    no strong objection at the time. The thinking was that it avoided adding
    a preemption point where one didn't exist before so the feedback was
    applied. This reasoning was wrong.
    
    There was an indirect preemption point as explained by Thomas Gleixner where
    an interrupt could set_need_resched() followed by preempt_enable being
    a preemption point that matters. This use of preempt_enable_no_resched
    is bad from both a mainline and RT perspective and a violation of the
    preemption mechanism. Peter Zijlstra noted that "the only acceptable use
    of preempt_enable_no_resched() is if the next statement is a schedule()
    variant".
    
    The usage was outright broken and I should have stuck to preempt_enable()
    as it was originally developed. It's known from previous tests
    that there was no detectable difference to the performance by using
    preempt_enable_no_resched().
    
    This is a fix to the mmotm patch
    mm-page_alloc-only-use-per-cpu-allocator-for-irq-safe-requests.patch
    
    Link: http://lkml.kernel.org/r/20170208143128.25ahymqlyspjcixu@techsingularity.net
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Reviewed-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Hillf Danton <hillf.zj@alibaba-inc.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2517,7 +2517,7 @@ void free_hot_cold_page(struct page *page, bool cold)
 	}
 
 out:
-	preempt_enable_no_resched();
+	preempt_enable();
 }
 
 /*
@@ -2683,7 +2683,7 @@ static struct page *rmqueue_pcplist(struct zone *preferred_zone,
 		__count_zid_vm_events(PGALLOC, page_zonenum(page), 1 << order);
 		zone_statistics(preferred_zone, zone);
 	}
-	preempt_enable_no_resched();
+	preempt_enable();
 	return page;
 }
 
