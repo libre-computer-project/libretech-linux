9920ed1461bbe072a815c352f016cd08c756fba3
Peter Rosin <peda@axentia.se>
2017-03-14 17:34:23 +0100

    mux: core: fix error handling in devm_mux_chip_alloc
    The error handling is mixed up. mux_chip_alloc() doesn't return an error
    pointer (just NULL on failure), so check for NULL instead of using IS_ERR.
    devm_mux_chip_alloc is documented to return NULL on failure, so fix that
    as well.
    
    All users of devm_mux_chip_alloc() are coded according to documentation.
    
    Fixes: d47598cc669b ("mux: minimal mux subsystem and gpio-based mux controller")
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Peter Rosin <peda@axentia.se>

--- drivers/mux/mux-core.c
+++ drivers/mux/mux-core.c
@@ -166,12 +166,12 @@ struct mux_chip *devm_mux_chip_alloc(struct device *dev,
 
 	ptr = devres_alloc(devm_mux_chip_release, sizeof(*ptr), GFP_KERNEL);
 	if (!ptr)
-		return ERR_PTR(-ENOMEM);
+		return NULL;
 
 	mux_chip = mux_chip_alloc(dev, controllers, sizeof_priv);
-	if (IS_ERR(mux_chip)) {
+	if (!mux_chip) {
 		devres_free(ptr);
-		return mux_chip;
+		return NULL;
 	}
 
 	*ptr = mux_chip;
