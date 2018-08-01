4c05a1c39fd25e5f054ae858208518bf83237407
Neil Armstrong <narmstrong@baylibre.com>
2018-08-01 10:54:13 +0200

    meson: pinctrl-gxl: add support for TEST_N pin
    The Amlogic Meson GXL & GXM SoCs needs a Secure Monitor call to set the TEST_N
    direction, add a special bank to support this pin.

--- drivers/pinctrl/meson/pinctrl-meson-gxl.c
+++ drivers/pinctrl/meson/pinctrl-meson-gxl.c
@@ -9,6 +9,7 @@
 #include <dt-bindings/gpio/meson-gxl-gpio.h>
 #include "pinctrl-meson.h"
 #include "pinctrl-meson8-pmx.h"
+#include <linux/firmware/meson/meson_sm.h>
 
 static const struct pinctrl_pin_desc meson_gxl_periphs_pins[] = {
 	MESON_PIN(GPIOZ_0),
@@ -821,6 +822,9 @@ static struct meson_bank meson_gxl_periphs_banks[] = {
 static struct meson_bank meson_gxl_aobus_banks[] = {
 	/*   name    first      last      irq	pullen  pull    dir     out     in  */
 	BANK("AO",   GPIOAO_0,  GPIOAO_9, 0, 9, 0,  16, 0, 0,   0,  0,  0, 16,  1,  0),
+	/* This PIN Direction must be set using a SMC call, so dir reg offset is the sm call */
+	BANK_SMC("TEST", GPIO_TEST_N, GPIO_TEST_N,
+					 -1, -1, 0, 30,  0, 14, SM_TEST_N_DIR, 1, 0, 31, 1, 31),
 };
 
 static struct meson_pinctrl_data meson_gxl_periphs_pinctrl_data = {
