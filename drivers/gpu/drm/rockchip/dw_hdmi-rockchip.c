e215de56f180abb640a9337b77e9da30ec4ed3ee
Alex Bee <knaerzche@gmail.com>
2021-06-01 19:24:37 +0200

    drm/rockchip: allow 4096px width modes
    There is not reason to limit vop output to 3840px width modes.
    Also drop the limitation from dw_hdmi_rockchip_mode_valid, since
    the max dimenstions of the actual vop version is validated in
    vop_crtc_mode_valid anyways.
    
    Signed-off-by: Alex Bee <knaerzche@gmail.com>

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -349,7 +349,7 @@ dw_hdmi_rockchip_mode_valid(struct dw_hdmi *hdmi, void *data,
 			return MODE_CLOCK_HIGH;
 	}
 
-	return drm_mode_validate_size(mode, 3840, 2160);
+	return MODE_OK;
 }
 static void
 dw_hdmi_rockchip_bridge_mode_set(struct drm_bridge *bridge,
