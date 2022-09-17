d8a86b7df968685d5c408ba36f640dce93ff36ea
Da Xue <da.xue@libretech.co>
2022-09-17 08:23:33 -0400

    meson venc: min 320 max 3840 h/v resolution

--- drivers/gpu/drm/meson/meson_venc.c
+++ drivers/gpu/drm/meson/meson_venc.c
@@ -867,10 +867,10 @@ meson_venc_hdmi_supported_mode(const struct drm_display_mode *mode)
 		return MODE_BAD;
 
 	/* support higher resolution than 1920x1080 */
-	if (mode->hdisplay < 640 || mode->hdisplay > 2560)
+	if (mode->hdisplay < 320 || mode->hdisplay > 3840)
 		return MODE_BAD_HVALUE;
 
-	if (mode->vdisplay < 480 || mode->vdisplay > 1600)
+	if (mode->vdisplay < 320 || mode->vdisplay > 3840)
 		return MODE_BAD_VVALUE;
 
 	return MODE_OK;
