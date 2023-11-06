c566e7c2a65f4f07881471e3057c9d417c5d5f18
Neil Armstrong <narmstrong@baylibre.com>
2023-11-06 23:43:23 +0000

    drm/meson: add cursor plane using OSD2
    Tested on A311D using modetest, Weston & Sway, should work on SM1 & G12A.
    
    Cursor doesn't move with Xorg, this would need some debugging.
    
    Also tested on GXL - the s905x SoC, which works fine.
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    Signed-off-by: Luke Lu <luke.lu@libre.computer>

---
+++ drivers/gpu/drm/meson/meson_cursor.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (C) 2021 BayLibre, SAS
+ * Author: Neil Armstrong <narmstrong@baylibre.com>
+ */
+
+#ifndef __MESON_CURSOR_H
+#define __MESON_CURSOR_H
+
+#include "meson_drv.h"
+
+int meson_cursor_create(struct meson_drm *priv);
+
+#endif /* __MESON_CURSOR_H */
