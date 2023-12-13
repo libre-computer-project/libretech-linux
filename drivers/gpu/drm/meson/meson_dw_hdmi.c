a91eea4635a27be3fab2aa60c005e6ee36491513
Luke Lu <luke.lu@libre.computer>
2023-12-13 03:47:44 +0000

    drm/meson: fix NULL pointer crash on resume without HDMI
    Only call dw_hdmi_poweron() during resume when the HDMI bridge is
    actually enabled. dw_hdmi_poweron() assumes hdmi->curr_conn is valid,
    which is only true when the bridge is enabled with a connected display.
    
    This prevents a NULL pointer dereference when resuming from suspend
    with the HDMI port disconnected.
    
    Signed-off-by: Luke Lu <luke.lu@libre.computer>

--- drivers/gpu/drm/meson/meson_dw_hdmi.c
+++ drivers/gpu/drm/meson/meson_dw_hdmi.c
@@ -809,7 +809,8 @@ static int __maybe_unused meson_dw_hdmi_pm_suspend(struct device *dev)
 	meson_dw_hdmi->data->top_write(meson_dw_hdmi,
 				       HDMITX_TOP_SW_RESET, 0);
 
-	dw_hdmi_poweroff(meson_dw_hdmi->hdmi);
+	if (dw_hdmi_is_bridge_on(meson_dw_hdmi->hdmi))
+		dw_hdmi_poweroff(meson_dw_hdmi->hdmi);
 
 	return 0;
 }
@@ -823,7 +824,9 @@ static int __maybe_unused meson_dw_hdmi_pm_resume(struct device *dev)
 
 	meson_dw_hdmi_init(meson_dw_hdmi);
 
-	dw_hdmi_poweron(meson_dw_hdmi->hdmi);
+	if (dw_hdmi_is_bridge_on(meson_dw_hdmi->hdmi))
+		dw_hdmi_poweron(meson_dw_hdmi->hdmi);
+
 	dw_hdmi_resume(meson_dw_hdmi->hdmi);
 
 	return 0;
